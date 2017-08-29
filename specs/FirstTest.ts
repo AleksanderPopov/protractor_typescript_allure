b50497e31f28c948b96596bf5ddf803f57c95247
Alexander Popov <aleksander.popov.93@gmail.com>
2017-08-29 14:59:17 +0300

    switch credentials to test pair

--- specs/FirstTest.ts
+++ specs/FirstTest.ts
@@ -4,15 +4,15 @@ import { LoginPage } from './../pageobjects/LoginPage';
 describe('protractorjs tests', function () {
   const loginPage: LoginPage = new LoginPage();
   const inboxPage: InboxPage = new InboxPage();
-  const email: string = "aleksander.popov.93@gmail.com";
-  const password: string = "";
+  const email: string = "ana.tjvi@gmail.com";
+  const password: string = "tjvitjvi";
   const messageTopic: string = "topic " + Date.now().toString();
 
   it('gmail test', async () => {
     await loginPage.open();
     await loginPage.loginAs(email, password);
     await inboxPage.composeEmail()
-              .then(async it => await it.withRecipients('aleksander.popov.93@gmail.com')
+              .then(async it => await it.withRecipients("ana.tjvi@gmail.com")
               .then(async it => await it.withTopic(messageTopic)
               .then(async it => await it.send())));
     await inboxPage.search(messageTopic);
